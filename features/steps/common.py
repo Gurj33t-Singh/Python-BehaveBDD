80af2e30d2d0dd31eef05467d579b59352636e99
Gurjeet Singh <gurjeets1803@gmail.com>
2024-03-14 00:52:02 +0530

    optimise code

--- features/steps/common.py
+++ features/steps/common.py
@@ -18,16 +18,16 @@ def step_impl(context, file_path):
     context.state_code=context.env_config['Environment']['stateCode']
     context.city_code=context.env_config['Environment']['cityCode']
 
-@given(u'Read API "{file_path}"')
-def step_impl(context, file_path):
-    """
-    gets json file as dictionary
-    """
-    context.api_config=utils.get_json(file_path)
-
-@given(u'Read constants "{file_path}"')
+@given(u'Read constants from "{file_path}"')
 def step_impl(context,file_path):
     context.constants=utils.get_json(file_path)
+
+@given(u'Read endpoints of "{key}"')
+def step_impl(context, key):
+    """
+    gets module specific API dictionary from endpoints_json file
+    """
+    context.endpoints=context.endpoints_json.get(key)
 
 @given(u'Prepare request headers with "{key}" constants')
 def step_impl(context, key):
@@ -39,24 +39,31 @@ def step_impl(context, key):
     # Prepared params
     context.params=context.constants[key]
 
-@when('Execute "{method}" request for "{endpoint}"')
-def step_impl(context, method, endpoint):
+@when('Execute "{method}" request for "{api_key}" api')
+def step_impl(context, method, api_key):
     
-    context.req=RequestBuilder()
-
+    # if headers is not set, then use blank dict
     if not hasattr(context, 'headers'):
         context.headers={}
 
+    # if params is not set, then use blank dict
     if not hasattr(context, 'params'):
         context.params={}
     
+    # if body is multipart encoded, then set content-type header
     if isinstance(context.body, requests_toolbelt.multipart.encoder.MultipartEncoder):        
         context.headers['content-type']=context.body.content_type
-        
+
+    # get specific api from endpoints of current module 
+    context.api=context.endpoints.get(api_key)
+    
+    # Create a RequestBuilder object to assign headers and params 
+    context.req=RequestBuilder()
     context.req.set_headers(context.headers)
     context.req.set_params(context.params)
+
     context.response=context.req.call_api(method=method, url=context.host,
-                                            api=endpoint, headers=context.req.headers,
+                                            api=context.api, headers=context.req.headers,
                                             params=context.req.params, data=context.body)
 
 @then('Response code "{resCode:d}"')
